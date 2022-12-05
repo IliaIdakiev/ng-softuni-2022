533f76563257d802448145c36802a0ae4298ce00
Iliya Idakiev <ilia.idakiev@gmail.com>
2022-12-05 16:29:12 +0100

    ngrx setup

--- 10/app/src/app/app-routing.module.ts
+++ 10/app/src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { ErrorComponent } from './core/error/error.component';
 import { HomeComponent } from './core/home/home.component';
